92273b88bf3817c733b18f96e3d40ab68fb85f2e
Ather Husain <Ather.Husain@ey.com>
2019-02-27 22:23:21 -0500

    Blank files for Trees

---
+++ AlgonDS/Trees/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgonDS
+{    
+    public class Node
+    {
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public string Value { get; set; }
+
+        public Node Insert(string value)
+        {
+            return new Node();
+        }
+
+        public bool Find(string value)
+        {
+            return true;
+        }
+    }
+}
