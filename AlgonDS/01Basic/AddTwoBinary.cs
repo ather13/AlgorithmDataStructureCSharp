a92a4428eb3aee70201acfd9c21b07e52340554c
Ather Husain <Ather.Husain@ey.com>
2019-03-10 15:02:48 -0400

    1. AddTwoBinary 2. Corrected project name

---
+++ AlgonDS/01Basic/AddTwoBinary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AlgonDS
+{
+    public class AddTwoBinary
+    {
+        //assuming both number has same length
+        public string Calculate(string num1, string num2)
+        {
+            var carry = 0;
+            var result = string.Empty;
+            for (var i = num1.Length - 1; i >= 0; i--)
+            {
+                var num = Char.GetNumericValue(num1[i]) + Char.GetNumericValue(num2[i]) + carry;
+                if (num > 1)
+                {
+                    num = 0;
+                    carry = 1;
+                }
+                result = num + result;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TestAlognDS/01Basic/AddTwoBinaryTests.cs
@@ -0,0 +1,27 @@
+﻿using AlgonDS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAlgonDS
+{
+    [TestClass]
+    public class AddTwoBinaryTests
+    {
+        [TestMethod]
+        public void CalculateSuccess()
+        {
+            var actual = (new AddTwoBinary()).Calculate("00001", "00010");
+
+            Assert.AreEqual("00011", actual);
+        }
+
+
+        [TestMethod]
+        public void Calculate2Success()
+        {
+            var actual = (new AddTwoBinary()).Calculate("01001", "01010");
+
+            Assert.AreEqual("10011", actual);
+        }
+
+    }
+}
