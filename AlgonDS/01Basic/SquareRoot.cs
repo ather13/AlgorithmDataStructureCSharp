7d779ec051b50953de256632fbda2c916151112d
Ather Husain <Ather.Husain@ey.com>
2019-03-10 15:26:28 -0400

    Square root of given number

---
+++ AlgonDS/01Basic/SquareRoot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgonDS
+{
+    public class SquareRoot
+    {
+        public int Getsquareroot(int n)
+        {
+            if (n == 0 || n == 1)
+                return n;
+
+            var sqrt = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                sqrt = i;
+                if (i * i == n)
+                {
+                    break;
+                }
+            }
+
+            return sqrt;
+        }
+    }
+}
---
+++ TestAlognDS/01Basic/SquareRootTests.cs
@@ -0,0 +1,25 @@
+﻿using AlgonDS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAlgonDS
+{
+    [TestClass]
+    public class SquareRootTests
+    {
+        [TestMethod]
+        public void SquareRootSuccess()
+        {
+            var actual = (new SquareRoot()).Getsquareroot(16);
+
+            Assert.AreEqual(4, actual);
+        }
+
+        [TestMethod]
+        public void SquareRootFailure()
+        {
+            var actual = (new SquareRoot()).Getsquareroot(15);
+
+            Assert.AreEqual(15, actual); //return same number if no square root found
+        }
+    }
+}
