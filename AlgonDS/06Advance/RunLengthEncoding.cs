ffcc070ec4187a3e108b6e0cb78954fe0c3ebf7c
Ather Husain <Ather.Husain@ey.com>
2019-03-17 00:04:32 -0400

    Run Length String

---
+++ AlgonDS/06Advance/RunLengthEncoding.cs
@@ -0,0 +1,47 @@
+﻿/// <summary>
+/// Given an input string, write a function that returns the Run Length Encoded string for the 
+/// input string.
+/// “wwwwaaadexxxxxx" => “w4a3d1e1x6”.
+/// </summary>
+
+namespace AlgonDS
+{
+    public class RunLengthEncodingDecoding
+    {
+        public string Encode(string value)
+        {
+            var c = value[0];
+            var counter = 1;
+            var output = string.Empty;
+            for (var i = 1; i < value.Length; i++)
+            {
+                if (c == value[i])
+                {
+                    counter++;
+                }
+                else
+                {
+                    output += c.ToString() + counter.ToString();
+                    c = value[i];
+                    counter = 1;
+                }
+            }
+            output += c.ToString() + counter.ToString();
+
+            return output;
+        }
+
+        public string Decode(string value)
+        {
+            if (string.IsNullOrEmpty(value) || value.Length == 1) return value;
+
+            var output = string.Empty;
+            for (int i = 0; i < value.Length; i+=2)
+            {
+                output += new string(value[i], (int)char.GetNumericValue(value[i + 1]));
+            }
+
+            return output;
+        }
+    }
+}
---
+++ TestAlognDS/06Advance/RunLengthEncodingTests.cs
@@ -0,0 +1,52 @@
+﻿using AlgonDS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAlgonDS
+{
+    [TestClass]
+    public class RunLengthEncodingDecodingTests
+    {
+        [TestMethod]
+        public void EncodeTest0()
+        {
+            var actual = new RunLengthEncodingDecoding().Encode("w");
+
+            Assert.AreEqual("w1", actual);
+        }
+
+        [TestMethod]
+        public void EncodeTest1()
+        {
+            var actual = new RunLengthEncodingDecoding().Encode("wwwwaaadexxxxxx");
+
+            Assert.AreEqual("w4a3d1e1x6", actual);
+        }
+
+        [TestMethod]
+        public void EncodeTest2()
+        {
+            var actual = new RunLengthEncodingDecoding().Encode("wwwwaaadexxxxxxywww");
+
+            Assert.AreEqual("w4a3d1e1x6y1w3", actual);
+        }
+
+        [TestMethod]
+        public void DecodeTest0()
+        {
+            var actual = new RunLengthEncodingDecoding().Decode("w1");
+
+            Assert.AreEqual("w", actual);
+        }
+
+        [TestMethod]
+        public void DecodeTest1()
+        {
+            var actual = new RunLengthEncodingDecoding().Decode("w4a3d1e1x6");
+
+            Assert.AreEqual("wwwwaaadexxxxxx", actual);
+        }
+    }
+}
