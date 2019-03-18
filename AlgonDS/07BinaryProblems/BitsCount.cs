3663d2f646e736e8e2aaa1926a563eceeb80de22
Ather Husain <Ather.Husain@ey.com>
2019-03-17 22:03:14 -0400

    Bit Counts based on given number

---
+++ AlgonDS/07BinaryProblems/BitsCount.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Linq;
+
+/// <summary>
+/// Given a positive integer n, count the total number of set bits 
+/// in binary representation of all numbers from 1 to n.
+/// 
+/// Example :
+/// 3 => 4
+/// 6 => 9
+/// </summary>
+namespace AlgonDS
+{
+    public class BitsCount
+    {
+        public int BitsCountInGivenNumber(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            var counter = 2;
+
+            for (var i = 3; i <= n; i++)
+            {
+                var binary = Convert.ToString(i, 2);
+                counter += binary.Count(x => char.GetNumericValue(x) == 1);
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ TestAlognDS/07BinaryProblems/BitsCountTests.cs
@@ -0,0 +1,17 @@
+﻿using AlgonDS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAlgonDS
+{
+    [TestClass]
+    public class BitsCountTests
+    {
+        [TestMethod]
+        public void BitsCountInGivenNumberTests()
+        {
+            var actual = new BitsCount().BitsCountInGivenNumber(6);
+
+            Assert.AreEqual(9, actual);
+        }
+    }
+}
