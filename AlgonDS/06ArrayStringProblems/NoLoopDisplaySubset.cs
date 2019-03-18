7bf1ee4f4927e5f52e6c433c85593cf41939fcde
Ather Husain <Ather.Husain@ey.com>
2019-03-17 20:42:41 -0400

    Print all subsets of given number n without using any loop

---
+++ AlgonDS/06ArrayStringProblems/NoLoopDisplaySubset.cs
@@ -0,0 +1,30 @@
+﻿
+/// <summary>
+/// Print all subsets of given number n without using any loop
+/// Example : 
+/// For given numer of 4
+/// {  1  2  3  4  }
+/// {  1  2  3  }
+/// {  1  2  }
+/// {  1  }
+/// </summary>
+
+namespace AlgonDS
+{
+    public class NoLoopDisplaySubset
+    {
+        public string PrintAll(int num)
+        {
+            if (num == 0) return "";
+            return $"{{ {PrintSubset(num - 1)} {num} }}\n{PrintAll(num - 1)}".TrimEnd('\n');
+
+            string PrintSubset(int subnum)
+            {
+                if (subnum == 0) return "";
+                return $"{PrintSubset(subnum - 1)} {subnum} ";
+            }
+        }
+
+        
+    }
+}
---
+++ TestAlognDS/06ArrayStringProblems/NoLoopDisplaySubsetTests.cs
@@ -0,0 +1,19 @@
+﻿using AlgonDS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAlgonDS
+{
+    [TestClass]
+    public class NoLoopDisplaySubsetTests
+    {
+        [TestMethod]
+        public void PrintSubsetTests()
+        {
+            var actual = new NoLoopDisplaySubset().PrintAll(4);
+
+            var expected = "{  1  2  3  4 }\n{  1  2  3 }\n{  1  2 }\n{  1 }";
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
