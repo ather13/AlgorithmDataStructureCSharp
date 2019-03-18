8316e76afe659cf62c8079abf4c2828740748603
Ather Husain <Ather.Husain@ey.com>
2019-03-17 21:16:54 -0400

    Conversion From Decimal to Binary

---
+++ AlgonDS/01Basic/Conversions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgonDS
+{
+    public class Conversions
+    {
+        public string DecimalToBinary(int n)
+        {
+            if (n < 2) return n.ToString();
+
+            var result = string.Empty;
+
+            while (n > 0)
+            {
+                var bin = n % 2;
+                n = n / 2;
+
+                result += bin;
+            }
+            return result;
+        }
+
+        public string BCL_DecimalToBinary(int n)
+        {
+            return Convert.ToString(n, 2);
+        }
+    }
+}
---
+++ TestAlognDS/01Basic/ConversionsTests.cs
@@ -0,0 +1,21 @@
+﻿using AlgonDS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAlgonDS
+{
+    [TestClass]
+    public class ConversionsTests
+    {
+        [TestMethod]
+        public void DecimalToBinaryTests()
+        {
+            var expected = new Conversions().BCL_DecimalToBinary(5);
+            var actual = new Conversions().DecimalToBinary(5);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
