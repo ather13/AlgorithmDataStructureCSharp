113cac7c342fb4df637dc3b0f2e9a8c76267ffd6
Ather Husain <Ather.Husain@ey.com>
2019-04-06 05:57:34 -0400

    Identity Matrix

---
+++ AlgonDS/10MatrixProblems/IdentityMatrix.cs
@@ -0,0 +1,31 @@
+﻿/// <summary>
+/// In linear algebra, the identity matrix, or sometimes ambiguously called a unit matrix, 
+/// of size n is the n × n square matrix with ones on the main diagonal and zeros elsewhere.
+/// Hint : When we multiple any matrix with identity matrix we should get same matrix back (I.X => X)
+/// 2X2 =>  [1,0]
+///         [0,1]
+/// 3X3 =>  [1,0,0]
+///         [0,1,0]
+///         [0,0,1]
+/// </summary>
+
+namespace AlgonDS
+{
+    public class IdentityMatrix
+    {
+        public bool IsIdentityMatrix(int[,] arr)
+        {
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                {
+                    if((i==j && arr[i,j] ==0) || (i!=j && arr[i, j] == 1)){
+                        return false;
+                    }
+                }
+            }
+            
+            return true;
+        }
+    }
+}
---
+++ TestAlognDS/10MatrixProblems/IdentityMatrixTests.cs
@@ -0,0 +1,49 @@
+﻿using AlgonDS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAlgonDS
+{
+    [TestClass]
+    public class IdentityMatrixTests
+    {
+        [TestMethod]
+        public void Verify2X2Matrix_Success()
+        {
+            var matrix = new int[,] { { 1, 0 },{ 0, 1 } };
+
+            var actual = new IdentityMatrix().IsIdentityMatrix(matrix);
+
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod]
+        public void Verify3X3Matrix_Success()
+        {
+            var matrix = new int[,] { { 1, 0,0 }, { 0, 1,0 }, { 0, 0, 1 } };
+
+            var actual = new IdentityMatrix().IsIdentityMatrix(matrix);
+
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod]
+        public void Verify2X2Matrix_AllOnes_Failure()
+        {
+            var matrix = new int[,] { { 1, 1 }, { 1, 1 } };
+
+            var actual = new IdentityMatrix().IsIdentityMatrix(matrix);
+
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod]
+        public void Verify2X2Matrix_Reverse_Failure()
+        {
+            var matrix = new int[,] { { 0, 1 }, { 1, 0 } };
+
+            var actual = new IdentityMatrix().IsIdentityMatrix(matrix);
+
+            Assert.AreEqual(false, actual);
+        }
+    }
+}
