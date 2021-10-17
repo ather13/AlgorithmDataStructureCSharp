dd6b54a30167911bdb6a9fe406ab7ad713d1c4f0
Ather Husain <ather.husain@assurant.com>
2021-10-17 17:34:34 -0400

    HasPath problem added

---
+++ AlgonDS/11GraphProblems/HasPathProblem.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgonDS._11GraphProblems
+{
+    /// <summary>
+    /// Find if two given nodes has path in directed acyclic graph 
+    /// </summary>
+    public class HasPathProblem
+    {
+        /// <summary>
+        /// Verify path using Depth First Algorithm - Recursive
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public bool HasPathUsingDfsRecursive(Dictionary <char, char[]> graph, char source, char target)
+        {
+            if (graph == null || !graph.Any()) return false;
+
+            if (source == default(char) || target == default(char)) return false;
+
+            if (source == target) return true;
+
+            foreach (var neighbor in graph[source])
+            {
+                if (HasPathUsingDfsRecursive(graph, neighbor, target))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Verify path using Depth First Algorithm - Non Recursive
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public bool HasPathUsingDfs(Dictionary<char, char[]> graph, char source, char target)
+        {
+            if (graph == null || !graph.Any()) return false;
+
+            if (source == default(char) || target == default(char)) return false;
+
+            var stack = new Stack<char>();
+            stack.Push(source);
+
+            while (stack.Any())
+            {
+                var current = stack.Pop();
+
+                if (current == target) return true;
+
+                foreach (var neighbor in graph[current])
+                {
+                    stack.Push(neighbor);
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Verify path using Breath First Algorithm
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public bool HasPathUsingBfs(Dictionary<char, char[]> graph, char source, char target)
+        {
+            if (graph == null || !graph.Any()) return false;
+
+            if (source == default(char) || target == default(char)) return false;
+            
+            var queue = new Queue<char>();
+            queue.Enqueue(source);
+
+            while (queue.Any())
+            {
+                var current = queue.Dequeue();
+
+                if (current == target) return true;
+
+                foreach (var neighbor in graph[current])
+                {
+                    queue.Enqueue(neighbor);
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ TestAlognDS/11GraphProblems/HasPathProblemTests.cs
@@ -0,0 +1,133 @@
+﻿using AlgonDS._11GraphProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace TestAlgonDS._11GraphProblems
+{
+    [TestCategory("Graph Algorithm")]
+    [TestClass]
+    public class HasPathProblemTests
+    {
+        public readonly HasPathProblem _sut;
+
+        public HasPathProblemTests()
+        {
+            _sut = new HasPathProblem();
+        }
+
+        [TestMethod]
+        public void ShouldSuccessWhenProvidedPathExists_DFS()
+        {
+            var graph = new Dictionary<char, char[]>()
+            {
+                { 'f',new char[] {'g', 'i'} },
+                { 'g',new char[] {'h'} },
+                { 'h',Array.Empty<char>()},
+                { 'i',new char[] {'g', 'k'} },
+                { 'j',new char[] {'i'} },
+                { 'k',Array.Empty<char>()},
+            };
+
+
+            var actual = _sut.HasPathUsingDfs(graph, 'f', 'k');
+
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod]
+        public void ShouldFailWhenProvidedPathNotExists_DFS()
+        {
+            var graph = new Dictionary<char, char[]>()
+            {
+                { 'f',new char[] {'g', 'i'} },
+                { 'g',new char[] {'h'} },
+                { 'h',Array.Empty<char>()},
+                { 'i',new char[] {'g', 'k'} },
+                { 'j',new char[] {'i'} },
+                { 'k',Array.Empty<char>()},
+            };
+
+
+            var actual = _sut.HasPathUsingDfs(graph, 'f', 'c');
+
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod]
+        public void ShouldSuccessWhenProvidedPathExists_DFSRecursive()
+        {
+            var graph = new Dictionary<char, char[]>()
+            {
+                { 'f',new char[] {'g', 'i'} },
+                { 'g',new char[] {'h'} },
+                { 'h',Array.Empty<char>()},
+                { 'i',new char[] {'g', 'k'} },
+                { 'j',new char[] {'i'} },
+                { 'k',Array.Empty<char>()},
+            };
+
+
+            var actual = _sut.HasPathUsingDfsRecursive(graph, 'f', 'k');
+
+            Assert.AreEqual(true,actual);
+        }
+
+        [TestMethod]
+        public void ShouldFailWhenProvidedPathNotExists_DFSRecursive()
+        {
+            var graph = new Dictionary<char, char[]>()
+            {
+                { 'f',new char[] {'g', 'i'} },
+                { 'g',new char[] {'h'} },
+                { 'h',Array.Empty<char>()},
+                { 'i',new char[] {'g', 'k'} },
+                { 'j',new char[] {'i'} },
+                { 'k',Array.Empty<char>()},
+            };
+
+
+            var actual = _sut.HasPathUsingDfsRecursive(graph, 'f', 'c');
+
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod]
+        public void ShouldSuccessWhenProvidedPathExists_BFS()
+        {
+            var graph = new Dictionary<char, char[]>()
+            {
+                { 'f',new char[] {'g', 'i'} },
+                { 'g',new char[] {'h'} },
+                { 'h',Array.Empty<char>()},
+                { 'i',new char[] {'g', 'k'} },
+                { 'j',new char[] {'i'} },
+                { 'k',Array.Empty<char>()},
+            };
+
+
+            var actual = _sut.HasPathUsingBfs(graph, 'f', 'k');
+
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod]
+        public void ShouldFailWhenProvidedPathNotExists_BFS()
+        {
+            var graph = new Dictionary<char, char[]>()
+            {
+                { 'f',new char[] {'g', 'i'} },
+                { 'g',new char[] {'h'} },
+                { 'h',Array.Empty<char>()},
+                { 'i',new char[] {'g', 'k'} },
+                { 'j',new char[] {'i'} },
+                { 'k',Array.Empty<char>()},
+            };
+
+
+            var actual = _sut.HasPathUsingBfs(graph, 'f', 'c');
+
+            Assert.AreEqual(false, actual);
+        }
+    }
+}
