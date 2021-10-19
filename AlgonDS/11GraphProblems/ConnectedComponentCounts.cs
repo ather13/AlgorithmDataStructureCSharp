e10d340c780bcae0176cb55af231f6ee71754ed9
Ather Husain <ather.husain@assurant.com>
2021-10-18 23:10:56 -0400

    component count graph algorithm

---
+++ AlgonDS/11GraphProblems/ConnectedComponentCounts.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace AlgonDS._11GraphProblems
+{
+    public class ConnectedComponentCounts
+    {
+        /// <summary>
+        /// Get count of components which are disconnected
+        /// Used Visited as graph is undirected / bidirectional graph
+        /// </summary>
+        /// <param name="graph"></param>
+        public int GetConnectedComponentCounts(Dictionary<int,int[]> graph)
+        {
+            var visited = new HashSet<int>();
+            var count = 0;
+
+            foreach (var node in graph)
+            {
+                if (explore(graph, node.Key, visited))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private bool explore(Dictionary<int, int[]> graph, int node,HashSet<int> visited)
+        {
+            if (visited.Contains(node)) return false;
+
+            visited.Add(node);
+
+            foreach (var neighbor in graph[node])
+            {
+                explore(graph, neighbor, visited);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ TestAlognDS/11GraphProblems/ConnectedComponentCountsTests.cs
@@ -0,0 +1,34 @@
+﻿using AlgonDS._11GraphProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace TestAlgonDS._11GraphProblems
+{
+    public class ConnectedComponentCountsTests
+    {
+        public readonly ConnectedComponentCounts _sut;
+
+        public ConnectedComponentCountsTests()
+        {
+            _sut = new ConnectedComponentCounts();
+        }
+
+        public void ShouldReturnComponentsCount()
+        {
+            var graph = new Dictionary<int, int[]>()
+            {
+                { 0, new int[] { 8, 1, 5 } },
+                { 1, new int[] { 0 } },
+                { 5, new int[] { 0, 8 } },
+                { 8, new int[] { 0, 5 } },
+                { 2, new int[] { 3, 4 } },
+                { 3, new int[] { 2, 4 } },
+                { 4, new int[] { 3, 2 } }
+            };
+
+            var count = _sut.GetConnectedComponentCounts(graph);
+
+            Assert.AreEqual(3, count);
+        }
+    }
+}
