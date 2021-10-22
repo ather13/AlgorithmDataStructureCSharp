17385d00bebba82591b1fac29116b13708c5ee0d
Ather Husain <ather.husain@assurant.com>
2021-10-22 15:02:40 -0400

    graph - min island size

---
+++ AlgonDS/11GraphProblems/MinIslandSizeProblem.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace AlgonDS._11GraphProblems
+{
+    public class MinIslandSizeProblem
+    {
+        public int GetMinIslandCount(char[][] grid)
+        {
+            var visited = new HashSet<string>();
+            var minSize = int.MaxValue;
+
+            for (int row = 0; row < grid.Length; row++)
+            {
+                for (int col = 0; col < grid[row].Length; col++)
+                {
+                    var size = GetIslandCount(grid, row, col, visited);
+
+                    if (minSize > size && size != 0)
+                    {
+                        minSize = size;
+                    }
+
+                }
+            }
+
+            return minSize;
+        }
+
+        private int GetIslandCount(char[][] grid, int row, int col, ISet<string> visited)
+        {
+            
+
+            var inBoundRow = row >= 0 && row < grid.Length;
+            var inBoundCol = col >= 0 && col < grid[0].Length;
+
+            if (!inBoundRow || !inBoundCol) return 0;
+
+            if (grid[row][col] == 'W') return 0;
+
+            if (visited.Contains($"{row}-{col}")) return 0;
+
+            visited.Add($"{row}-{col}");
+
+            var size = 1;
+            size += GetIslandCount(grid, row + 1, col, visited);
+            size += GetIslandCount(grid, row - 1, col, visited);
+            size += GetIslandCount(grid, row, col + 1, visited);
+            size += GetIslandCount(grid, row, col - 1, visited);
+
+            return size;
+        }
+    }
+}
---
+++ TestAlognDS/11GraphProblems/MinIslandSizeTests.cs
@@ -0,0 +1,34 @@
+﻿using AlgonDS._11GraphProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAlgonDS._11GraphProblems
+{
+    [TestClass]
+    public class MinIslandSizeTests
+    {
+        private readonly MinIslandSizeProblem _sut;
+
+        public MinIslandSizeTests()
+        {
+            _sut = new MinIslandSizeProblem();
+        }
+
+        [TestMethod]
+        public void GetMinIslandSize()
+        {
+            var grid = new char[6][]
+            {
+                new char[] {'W','L','W','W','W' },
+                new char[] {'W','L','W','W','W' },
+                new char[] {'W','W','W','L','W' },
+                new char[] {'W','W','L','L','W' },
+                new char[] {'L','W','W','L','L' },
+                new char[] {'L','L','W','W','W' },
+            };
+
+            var count = _sut.GetMinIslandCount(grid);
+
+            Assert.AreEqual(2, count);
+        }
+    }
+}
