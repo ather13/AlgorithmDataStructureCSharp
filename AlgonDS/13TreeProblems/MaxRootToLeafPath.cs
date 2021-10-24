d7d3bc005ff429cedc580ef76d84476057ffe348
Ather Husain <ather.husain@assurant.com>
2021-10-24 17:52:32 -0400

    Tree - Max Sum from root to leaf

---
+++ AlgonDS/13TreeProblems/MaxRootToLeafPath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AlgonDS._12Tree;
+
+namespace AlgonDS._13TreeProblems
+{
+    public class MaxRootToLeafPath
+    {
+        /// <summary>
+        /// Find max sum of node from root to leaf 
+        /// </summary>
+        public int GetMaxRootToLeafPath(TreeNode<int> root)
+        {
+            if (root == null) return 0;
+
+            var maxValue =  Math.Max(GetMaxRootToLeafPath(root.Right),  GetMaxRootToLeafPath(root.Left));
+
+            return maxValue + root.Data;
+        }
+    }
+}
---
+++ TestAlognDS/13TreeProblems/MaxRootToLeafPathTests.cs
@@ -0,0 +1,38 @@
+﻿using AlgonDS._12Tree;
+using AlgonDS._13TreeProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAlgonDS._13TreeProblems
+{
+    [TestClass]
+    public class MaxRootToLeafPathTests
+    {
+        private readonly MaxRootToLeafPath _sut;
+
+        public MaxRootToLeafPathTests()
+        {
+            _sut = new MaxRootToLeafPath();
+        }
+
+        [TestMethod]
+        public void ShouldReturnMaxLeafToPath()
+        {
+            var root = new TreeNode<int>(5);
+            var b = new TreeNode<int>(11);
+            var c = new TreeNode<int>(3);
+            var d = new TreeNode<int>(4);
+            var e = new TreeNode<int>(2);
+            var f = new TreeNode<int>(1);
+
+            root.Left = b;
+            root.Right = c;
+            b.Left = d;
+            b.Right = e;
+            c.Left = f;
+
+            var result = _sut.GetMaxRootToLeafPath(root);
+
+            Assert.AreEqual(20,result);
+        }
+    }
+}
