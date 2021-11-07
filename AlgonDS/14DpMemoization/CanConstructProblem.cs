3b589e903a72f566faad4ae48f8392a67ce5efbf
Ather Husain <ather.husain@assurant.com>
2021-11-06 21:01:57 -0400

    DA - Can Construct Problem

---
+++ AlgonDS/14DpMemoization/CanConstructProblem.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgonDS._14DpMemoization
+{
+    /// <summary>
+    /// Decision Problem
+    /// </summary>
+    public class CanConstructProblem
+    {
+        /// <summary>
+        /// Brute force - Recursive
+        /// Space : O(m^2)
+        /// Time  : O(n^m * m)
+        /// m = target word, n = length of word bank
+        /// </summary>
+        public bool CanConstructiveRecursive(string target, string[] wordBank)
+        {
+            if (string.IsNullOrEmpty(target)) return true;
+
+            foreach (var word in wordBank)
+            {
+                var index = target.IndexOf(word, StringComparison.OrdinalIgnoreCase);
+                
+                if (index == 0)
+                {
+                    var newTarget =  target[word.Length..]; // similar to "target.Substring(word.Length);"
+
+                    if (CanConstructiveRecursive(newTarget, wordBank))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Brute force - Recursive Memo
+        /// Space : O(m^2)
+        /// Time  : O(n * m^2)
+        /// m = target word, n = length of word bank
+        /// </summary>
+        public bool CanConstructiveRecursiveMemo(string target, string[] wordBank,Dictionary<string,bool> memo = null)
+        {
+            memo ??= new Dictionary<string, bool>();
+            if (memo.ContainsKey(target)) return memo[target];
+
+            if (string.IsNullOrEmpty(target)) return true;
+
+            foreach (var word in wordBank)
+            {
+                var index = target.IndexOf(word, StringComparison.OrdinalIgnoreCase);
+
+                if (index == 0)
+                {
+                    var newTarget = target[word.Length..]; // similar to "target.Substring(word.Length);"
+
+                    if (CanConstructiveRecursiveMemo(newTarget, wordBank,memo))
+                    {
+                        memo.Add(newTarget,true);
+                        return true;
+                    }
+                }
+            }
+
+            memo.Add(target, false);
+            return false;
+        }
+    }
+}
---
+++ TestAlognDS/14DpMemoization/CanConstructProblemTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgonDS._14DpMemoization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAlgonDS._14DpMemoization
+{
+    [TestClass]
+    public class CanConstructProblemTests
+    {
+        private readonly CanConstructProblem _sut;
+
+        public CanConstructProblemTests()
+        {
+            _sut = new CanConstructProblem();
+        }
+
+        [TestMethod]
+        public void ShouldReturnTrueIfItConstructTarget()
+        {
+            var result= _sut.CanConstructiveRecursive("abcdef", new string[] { "ab", "abc", "cd", "def", "abcd" });
+            Assert.AreEqual(true,result);
+
+            result = _sut.CanConstructiveRecursiveMemo("abcdef", new string[] { "ab", "abc", "cd", "def", "abcd" });
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void ShouldReturnFalseIfCantConstructTarget()
+        {
+            var result = _sut.CanConstructiveRecursive("skateboard", new string[] { "bo", "rd", "ate", "t", "ska","sk","boar" });
+            Assert.AreEqual(false, result);
+
+            result = _sut.CanConstructiveRecursiveMemo("skateboard", new string[] { "bo", "rd", "ate", "t", "ska", "sk", "boar" });
+            Assert.AreEqual(false, result);
+        }
+    }
+}
